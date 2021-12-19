29acf83fdb5422bd0e85af26475cdd5fb4939d16
jcass1961 <julia.cassetti@gmail.com>
2021-12-19 20:25:37 -0300

    Subiendo cambios a los graficos

--- PaperTesis_Proyect/Code/ConModificacionAlejandro/graficoinvluencial3alfa-3n25.r
+++ PaperTesis_Proyect/Code/ConModificacionAlejandro/graficoinvluencial3alfa-3n25.r
@@ -65,16 +65,16 @@ pp1<-ggplot(datos1, aes(x=grilla)) +
                         values = c("Set1" ="dashed", "Set2"="twodash" ,"Set3"= "dotted","Set4"="longdash"),
                         labels = legenda.nomb)+
   theme_few()+
-  theme(text=element_text(size=35, family="serif"),
+  theme(text=element_text(size=45, family="serif"),
         legend.position="top",
-        legend.text = element_text( size=35),
-        legend.title = element_text( size=35),
-        axis.text.y = element_text( size = 35 ),
+        legend.text = element_text( size=45),
+        legend.title = element_text( size=45),
+        axis.text.y = element_text( size = 45 ),
         axis.text.x = element_text(colour = c("red","red",rep("black",cant.black)),
-                                   angle=70,hjust = 1, size = 35),
-        axis.title.y = element_text( size = 35 ),
-        axis.title.x = element_text( size = 35 ),
-        strip.text = element_text(size = 35))
+                                   angle=70,hjust = 1, size = 45),
+        axis.title.y = element_text( size = 45 ),
+        axis.title.x = element_text( size = 45 ),
+        strip.text = element_text(size = 45))
 
 
 
